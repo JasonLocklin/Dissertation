6d67b69d329dc4c7d0edf550571254d6854b130c
Jason Locklin <jalockli@uwaterloo.ca>
2013-10-05 21:32:32 -0400

    General outline of the paper

---
+++ outline.m
@@ -0,0 +1,21 @@
+# Saccadic Adaptation and Spatial Perception
+
+
+
+## Traditional Saccadic Adaptation Approach
+
+
+
+## Pre and Post Saccadic Perceptual Effects
+
+
+
+## Lasting Perceptual Effects
+
+
+
+## Saccadic Adaptation and Perception in Clinical Populations
+
+
+
+## Conclusions
