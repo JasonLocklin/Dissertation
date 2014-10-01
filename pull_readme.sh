396c0179121a1786b7433a8f489113b2fb2540dc
Jason Locklin <jalockli@uwaterloo.ca>
2014-10-01 13:59:52 -0400

    instructions for moving files between repos

---
+++ pull_readme.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# WARNING: Do not edit README file in this repo.
+# Edit the readme in Phd-Publish, then pull with 
+# this command. I don't want to accidentally push
+# this working repo to the published one.
+
+echo "Pulling from published repo"
+git pull publish master
+
