13986130e7bdd22cfd7585f4d8552144cdc7dddd
Jason Locklin <jalockli@uwaterloo.ca>
2014-10-12 21:17:06 -0400

    Added "total changes" and added -b -w and --minimal to git diff command to avoid counting whitespace changes and transpositions in word counts.

--- git-wc.sh
+++ git-wc.sh
@@ -8,7 +8,8 @@ if [ "${1:0:2}" == '-h' ]; then
     ARG=" HEAD$HEAD..HEAD "
 fi
 
-REM=`git diff --word-diff=porcelain | grep '^-' | grep -v '\-\-\-' | sed 's/^-//' | wc -w`
-ADD=`git diff --word-diff=porcelain | grep '^+' | grep -v '+++' | sed 's/^+//' | wc -w`
+REM=`git diff --minimal -w -b --word-diff=porcelain | grep '^-' | grep -v '\-\-\-' | sed 's/^-//' | wc -w`
+ADD=`git diff --minimal -w -b --word-diff=porcelain | grep '^+' | grep -v '+++' | sed 's/^+//' | wc -w`
 DIF=`expr $ADD - $REM`
-echo "New words: $ADD, Removed words: $REM, Net change: $DIF"
+TOT=`expr $ADD + $REM`
+echo "New words: $ADD, Removed words: $REM, Net change: $DIF, Total change: $TOT"
